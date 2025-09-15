fc7bb9b899b09499e258f9924c6885c20a4ce0f7
hyouku <nurohmanekoaji1@gmail.com>
2025-09-15 13:28:14 +0700

    fix: rename Navbar path case-sensitive

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App.jsx'
-import Navbar from './Components/navbar.jsx'
+import Navbar from './Components/Navbar.jsx'
 import 'remixicon/fonts/remixicon.css'
 import Footer from './Components/Footer.jsx'
 import PreLoader from './Components/PreLoader.jsx'
